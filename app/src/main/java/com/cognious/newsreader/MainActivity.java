929968e1e4361f93278050175810040d6c89287c
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-08-09 21:43:51 -0500

    Added news list for India

--- app/src/main/java/com/cognious/newsreader/MainActivity.java
+++ app/src/main/java/com/cognious/newsreader/MainActivity.java
@@ -2,12 +2,67 @@ package com.cognious.newsreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> values = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list);
+
+        RequestQueue reqQueue = Volley.newRequestQueue(this);
+
+        Date date = new Date();
+        String modifiedDate= new SimpleDateFormat("yyyyMMdd").format(date);
+        String url = "https://cognious.com/news/api/india/" + modifiedDate;
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        listView.setAdapter(arrayAdapter);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    for (int i = 0; i < response.getJSONArray("articles").length(); i++) {
+                        JSONObject articleObj = response.getJSONArray("articles").getJSONObject(i);
+                        String title = articleObj.getString("title");
+                        values.add(title);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
