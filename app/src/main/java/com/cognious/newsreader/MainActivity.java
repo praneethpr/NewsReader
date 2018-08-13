afe04d612fcaeaa885c41fcd8a1bae753bf77886
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-08-13 09:26:28 -0500

    Added image loader to volley singleton class and sample code to get image

--- app/src/main/java/com/cognious/newsreader/MainActivity.java
+++ app/src/main/java/com/cognious/newsreader/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cognious.newsreader;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.LruCache;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,48 +25,67 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> values = new ArrayList<String>();
 
+    private RequestQueue reqQueue;
+
+    private ImageLoader imageLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.list);
+//        ListView listView = findViewById(R.id.list);
 
-        RequestQueue reqQueue = Volley.newRequestQueue(this);
+        reqQueue = Volley.newRequestQueue(this);
 
         Date date = new Date();
         String modifiedDate= new SimpleDateFormat("yyyyMMdd").format(date);
         String url = "https://cognious.com/news/api/india/" + modifiedDate;
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+//        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
 
-        listView.setAdapter(arrayAdapter);
+//        listView.setAdapter(arrayAdapter);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-
-                try {
-                    for (int i = 0; i < response.getJSONArray("articles").length(); i++) {
-                        JSONObject articleObj = response.getJSONArray("articles").getJSONObject(i);
-                        String title = articleObj.getString("title");
-                        values.add(title);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                arrayAdapter.notifyDataSetChanged();
-
+        imageLoader = new ImageLoader(reqQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
             }
         });
 
+        NetworkImageView avatar = findViewById(R.id.thumbnail);
+        avatar.setImageUrl("https://www.gstatic.com/webp/gallery3/1.png",imageLoader);
 
-        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//                try {
+//                    for (int i = 0; i < response.getJSONArray("articles").length(); i++) {
+//                        JSONObject articleObj = response.getJSONArray("articles").getJSONObject(i);
+//                        String title = articleObj.getString("title");
+//                        values.add(title);
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                arrayAdapter.notifyDataSetChanged();
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//
+//            }
+//        });
+
+
+        VolleySingleton.getInstance(this).getRequestQueue();
+//        reqQueue = VolleySingleton.getInstance(getApplicationContext()).getRequestQueue();
     }
 }
--- app/src/main/java/com/cognious/newsreader/VolleySingleton.java
+++ app/src/main/java/com/cognious/newsreader/VolleySingleton.java
@@ -1,8 +1,11 @@
 package com.cognious.newsreader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 public class VolleySingleton {
@@ -10,10 +13,12 @@ public class VolleySingleton {
     private static VolleySingleton ourInstance;
     private RequestQueue mRequestQueue;
     private static Context mCtx;
+    private ImageLoader mImageLoader;
 
     private VolleySingleton(Context context) {
         mCtx = context;
         mRequestQueue = getRequestQueue();
+        mImageLoader = getImageLoader();
     }
 
     public static synchronized VolleySingleton getInstance(Context context) {
@@ -31,6 +36,26 @@ public class VolleySingleton {
             mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
         }
         return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
+                private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return mCache.get(url);
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                    mCache.put(url, bitmap);
+                }
+            });
+        }
+
+        return this.mImageLoader;
     }
 
     public <T> void addToRequestQueue(Request<T> request) {
